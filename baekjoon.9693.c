a3400a728a104b9ecf8cc7a2817171f0ebc2b2bc
exceed96 <exceed_96@naver.com>
2022-01-21 15:24:50 +0900

    BOJ.9693

---
+++ baekjoon.9693.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*수정*/
+int main(void)
+{
+	int i = 1;
+	while(1)
+	{
+		int N, r = 0;
+		scanf("%d", &N);
+		if(!N) break;
+		else
+		{
+			while(N)
+			{
+				r += N / 5;
+				N /= 5;
+			}
+		}
+		printf("Case #%d: %d\n",i,r);
+		i++;
+	}
+	return 0;
+}
