073fe68af7acb64caea9841ba0088522b0b81831
exceed96 <90549959+exceed96@users.noreply.github.com>
2022-10-19 18:40:34 +0900

    Create baekjoon.15651.cpp

---
+++ baekjoon.15651.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int n,m;
+int arr[10];
+//bool isused[10]; //특정 수가 쓰였는지를 true 혹은 false로 나타내는 배열
+
+void func(int k)
+{
+    if (k == m)
+    {
+        for(int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << "\n";
+        return ;
+    }
+    for(int i = 1; i <= n; i++)
+    {
+        //if(!isused[i])
+        //{
+            arr[k] = i;
+            //isused[i] = 1;
+            func(k + 1);
+            //isused[i] = 0;
+        //}
+    }
+}
+int main(void)
+{
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    cin >> n >> m;
+    func(0);
+}
