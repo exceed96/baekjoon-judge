315b082e7d284bcca6973836e4830eeb2b540e7d
exceed96 <90549959+exceed96@users.noreply.github.com>
2022-11-23 22:11:26 +0900

    Create baekjoon.6798.cpp

---
+++ baekjoon.6798.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+int board[10][10];
+long long vis[10][10];
+
+int dx[] = {2,2,-2,-2,1,1,-1,-1};
+int dy[] = {1,-1,1,-1,2,-2,2,-2};
+
+int s_x,s_y,e_x,e_y;
+
+int main(void)
+{
+    memset(vis, -1, sizeof(vis));
+    cin >> s_x >> s_y >> e_x >> e_y;
+    if (s_x == e_x && s_y == e_y)
+    {
+        cout << 0;
+        return (0);
+    }
+    queue <pair<int,int>> Q;
+    Q.push({s_x,s_y});
+    vis[s_x][s_y] = 0;
+    while (!Q.empty())
+    {
+        pair <int,int> cur = Q.front(); Q.pop();
+        for(int dir = 0; dir < 8; dir++)
+        {
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+            if(nx < 1 || nx > 8 || ny < 1 || ny > 8) continue;
+            // if (vis[nx][ny] >= 0)
+            if (nx == e_x && ny == e_y){
+                cout << vis[cur.first][cur.second] + 1;
+                return (0);
+            }
+            vis[nx][ny] = vis[cur.first][cur.second] + 1;
+            Q.push({nx,ny});
+        }
+    }
+    cout << vis[e_x][e_y];
+}
